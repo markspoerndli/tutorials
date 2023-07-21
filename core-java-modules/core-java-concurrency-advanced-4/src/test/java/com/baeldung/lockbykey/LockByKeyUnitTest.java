058114db1c1dc403d566168095f40c4eaf55f38a
Mark Spoerndli <mark.spoerndli@ti8m.ch>
2023-07-21 14:49:27 +0200

    Add concurrency tests to verify that creation, locking and unlocking of LockByKey works correcly when performed concurrently on multiple threads.

--- core-java-modules/core-java-concurrency-advanced-4/src/test/java/com/baeldung/lockbykey/LockByKeyUnitTest.java
+++ core-java-modules/core-java-concurrency-advanced-4/src/test/java/com/baeldung/lockbykey/LockByKeyUnitTest.java
@@ -1,11 +1,15 @@
 package com.baeldung.lockbykey;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
 
 public class LockByKeyUnitTest {
     
@@ -102,5 +106,63 @@ public class LockByKeyUnitTest {
             assertTrue(anotherThreadWasExecuted.get());
         }
     }
+
+    private static class LockedIncrementor implements Runnable {
+        private final List<Integer> counters;
+        private final int numIncrements;
+
+        private LockedIncrementor(List<Integer> counters, int numIncrements) {
+            this.counters = counters;
+            this.numIncrements = numIncrements;
+        }
+
+        @Override
+        public void run() {
+            LockByKey lockByKey = new LockByKey();
+            for (int i = 0; i < numIncrements; i++) {
+                for (int j = 0; j < counters.size(); j++) {
+                    String key = "counter_" + j;
+                    try {
+                        lockByKey.lock(key);
+                        counters.set(j, counters.get(j) + 1);
+                    } finally {
+                        lockByKey.unlock(key);
+                    }
+                }
+            }
+        }
+    }
+
+    @Test
+    @Timeout(10)
+    void givenCounterGuardedByLockByKey_WhenMultipleThreadsIncrementTheCounterConcurrently_ThenCounterIsIncrementedConsistently() {
+        final int numIncrements = 1000;
+        final int numCounters = 5;
+        final int threadCount = 10;
+
+        List<Integer> counters = new ArrayList<>();
+        for (int i = 0; i < numCounters; i++) {
+            counters.add(0);
+        }
+
+        List<Thread> incrementors = new ArrayList<>();
+        for (int i = 0; i < threadCount; i++) {
+            incrementors.add(new Thread(new LockedIncrementor(counters, numIncrements)));
+        }
+        incrementors.forEach(Thread::start);
+        incrementors.forEach(thread -> {
+            try {
+                thread.join();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        });
+
+        //if locking worked correctly, each counter should have been incremented <numIncrements> times
+        counters.forEach(counterValue -> assertEquals(threadCount * numIncrements, counterValue));
+    }
+
+
+
 
 }
